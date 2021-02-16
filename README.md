405932021ed352e0257dd0f532d86723f37aacb0
Andrew Snodgrass <git@pointlander.info>
2021-02-15 20:36:47 -0700

    Added simulation

---
+++ README.md
@@ -0,0 +1,2 @@
+# 8 particle universe simulation
+![simulation](verse.gif?raw=true)
--- main.go
+++ main.go
@@ -60,40 +60,88 @@ func main() {
 	l1 := tf32.Softmax(tf32.Add(tf32.Mul(set.Get("aw1"), set.Get("particles")), set.Get("ab1")))
 	cost := tf32.Avg(tf32.Quadratic(set.Get("particles"), l1))
 
-	iterations, i, total := 30, 0, float32(0.0)
-	alpha, eta := float32(.3), float32(.3)
-	points := make(plotter.XYs, 0, iterations)
-	for i < iterations {
-		total = 0
-		start := time.Now()
-		set.Zero()
+	alpha, eta, iterations := float32(.3), float32(.3), 30
+	points, images := make(plotter.XYs, 0, iterations), &gif.GIF{}
+	var palette = []color.Color{
+		color.RGBA{0, 0, 0, 0xff},
+		color.RGBA{0xff, 0xff, 0xff, 0xff},
+		color.RGBA{0, 0, 0xff, 0xff},
+	}
+	total := float32(0.0)
+	for step := 0; step < 256; step++ {
+		i := 0
+		for i < iterations {
+			total = 0
+			start := time.Now()
+			set.Zero()
 
-		total += tf32.Gradient(cost).X[0]
-		norm := float32(0)
-		for _, p := range set.Weights {
-			for _, d := range p.D {
-				norm += d * d
+			total += tf32.Gradient(cost).X[0]
+			norm := float32(0)
+			for _, p := range set.Weights {
+				for _, d := range p.D {
+					norm += d * d
+				}
+			}
+			norm = float32(math.Sqrt(float64(norm)))
+			scaling := float32(1)
+			if norm > 1 {
+				scaling = 1 / norm
+			}
+
+			for k, p := range set.Weights {
+				for l, d := range p.D {
+					deltas[k][l] = alpha*deltas[k][l] - eta*d*scaling
+					p.X[l] += deltas[k][l]
+				}
+			}
+
+			points = append(points, plotter.XY{X: float64(i), Y: float64(total)})
+			fmt.Println(i, total, time.Now().Sub(start))
+			i++
+		}
+
+		particles := set.Weights[2]
+		verse := image.NewPaletted(image.Rect(0, 0, 800, 800), palette)
+		for i := 0; i < len(particles.X); i += Width {
+			maxX, maxY, max := 0, 0, float32(0.0)
+			for y := 0; y < 8; y++ {
+				offset := i + 8*y
+				for x := 0; x < 8; x++ {
+					if a := particles.X[offset+x]; a > max {
+						maxX, maxY, max = x, y, a
+					}
+				}
+			}
+			maxX *= 100
+			maxY *= 100
+			for x := 0; x < 80; x++ {
+				for y := 0; y < 80; y++ {
+					var dx, dy float32 = 40 - float32(x), 40 - float32(y)
+					d := float32(math.Sqrt(float64(dx*dx+dy*dy))) / 40
+					if d < 1 {
+						verse.Set(maxX+x, maxY+y, color.RGBA{255, 255, 255, 255})
+					}
+				}
+			}
+
+			for x := 0; x < int(float64(step)*800.0/256.0); x++ {
+				for y := 790; y < 800; y++ {
+					verse.Set(x, y, color.RGBA{0, 0, 255, 255})
+				}
 			}
 		}
-		norm = float32(math.Sqrt(float64(norm)))
-		scaling := float32(1)
-		if norm > 1 {
-			scaling = 1 / norm
-		}
+		images.Image = append(images.Image, verse)
+		images.Delay = append(images.Delay, 100)
 
-		for k, p := range set.Weights {
-			for l, d := range p.D {
-				deltas[k][l] = alpha*deltas[k][l] - eta*d*scaling
-				p.X[l] += deltas[k][l]
+		for i := range set.Weights {
+			w := set.Weights[i]
+			for j := range w.X {
+				w.X[j] += float32(rand.NormFloat64() * .001)
 			}
 		}
-
-		points = append(points, plotter.XY{X: float64(i), Y: float64(total)})
-		fmt.Println(i, total, time.Now().Sub(start))
-		i++
 	}
 
-	err := set.Save("weights.w", total, i)
+	err := set.Save("weights.w", total, iterations)
 	if err != nil {
 		panic(err)
 	}
@@ -120,26 +168,12 @@ func main() {
 		panic(err)
 	}
 
-	particles := set.Weights[2]
-	verse := image.NewRGBA(image.Rect(0, 0, 8, 8))
-	for i := 0; i < len(particles.X); i += Width {
-		maxX, maxY, max := 0, 0, float32(0.0)
-		for y := 0; y < 8; y++ {
-			offset := i + 8*y
-			for x := 0; x < 8; x++ {
-				if a := particles.X[offset+x]; a > max {
-					maxX, maxY, max = x, y, a
-				}
-			}
-		}
-		verse.Set(maxX, maxY, color.RGBA{255, 255, 255, 0})
-	}
 	out, err := os.Create("verse.gif")
 	if err != nil {
 		panic(err)
 	}
 	defer out.Close()
-	err = gif.Encode(out, verse, nil)
+	err = gif.EncodeAll(out, images)
 	if err != nil {
 		panic(err)
 	}
